a4cf868150be805d9a1825d24c1a21254e183c71
shivkore <kores9214@gmail.com>
2018-12-22 10:48:09 +0530

    Assignment62 added

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment62';
+  fun(){
+    return "Marvellous Infosystems";
+  }
+}
